94424db450747d95bc486ac45d6cb2cb7be422d7
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-02-17 14:48:42 +0700

    fix error header sent

--- mdw/messenger.mdw.js
+++ mdw/messenger.mdw.js
@@ -21,14 +21,14 @@ module.exports = {
             if (mode === "subscribe" && token === VERIFY_TOKEN) {
               // Respond with the challenge token from the request
               console.log("WEBHOOK_VERIFIED");
-              res.status(200).send(challenge);
+              return res.status(200).send(challenge);
             } else {
               // Respond with '403 Forbidden' if verify tokens do not match
-              res.sendStatus(403);
+              return res.sendStatus(403);
             }
           }
         
-          res.sendStatus(404);
+          return res.sendStatus(404);
     },
     postWebHook:(req, res) => {  
 
